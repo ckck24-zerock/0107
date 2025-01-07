1413e85842601a09429345a132efb7fcbd5db008
zerock <kangckck24@gmail.com>
2025-01-07 11:35:19 +0900

    lotto

--- src/main/java/org/example/Lotto1.java
+++ src/main/java/org/example/Lotto1.java
@@ -9,13 +9,10 @@ public class Lotto1 {
         //결과물을 담을 배열 선언
         int[] result = new int[6];
 
+        for (int i = 0; i < 6; i++) {
+            result[i] = (int)(Math.random() * 45) + 1;
+        }
         //1 - 45 랜덤
-        result[0] = (int)(Math.random() * 45) + 1;
-        result[1] = (int)(Math.random() * 45) + 1;
-        result[2] = (int)(Math.random() * 45) + 1;
-        result[3] = (int)(Math.random() * 45) + 1;
-        result[4] = (int)(Math.random() * 45) + 1;
-        result[5] = (int)(Math.random() * 45) + 1;
 
         System.out.println(Arrays.toString(result));
     }
---
+++ src/main/java/org/example/Lotto2.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Lotto2 {
+
+    public static void main(String[] args) {
+
+        int[] balls = new int[45];
+
+        for (int i = 0; i < balls.length ; i++) {
+            balls[i] = i +1;
+        }
+        System.out.println(Arrays.toString(balls));
+
+        //임의의 숫자 0부터 44까지 - 6번 뽑기
+        for (int i = 0; i < 6; ) {
+
+            int idx = (int)(Math.random() * 45); //임의의 배열내 인덱스 번호
+
+            int value = balls[idx]; //내용물
+
+            //만일 value == -1이면 continue
+            if(value == -1){
+                System.out.println("중복 발생");
+                continue;
+            }
+
+            System.out.println(value);
+
+            //출력한 다음에는 내용물을 -1로 바꾼다.
+            balls[idx] = -1;
+            i++;
+        }
+
+        System.out.println(Arrays.toString(balls));
+
+    }
+}
