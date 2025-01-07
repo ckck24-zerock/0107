98255aec1ae9d3b476de77ceb7bcffbd461db247
zerock <kangckck24@gmail.com>
2025-01-07 16:28:22 +0900

    lotto

--- src/main/java/org/example/Lotto1.java
+++ src/main/java/org/example/Lotto1.java
@@ -10,7 +10,12 @@ public class Lotto1 {
         int[] result = new int[6];
 
         for (int i = 0; i < 6; i++) {
-            result[i] = (int)(Math.random() * 45) + 1;
+
+            int temp = (int)(Math.random() * 45) + 1;
+
+            //result배열안에 temp가 있는지 체크하는 로직을 구성
+
+            result[i] = temp;
         }
         //1 - 45 랜덤
 
